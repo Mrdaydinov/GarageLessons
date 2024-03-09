af496082f21f548a259e8bff1e3687304b17cc64
Murad <mrdaydinov@gmail.com>
2024-03-09 16:48:29 +0400

    LabTasks6 added

---
+++ LabTasks6/LabTasks6/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace LabTasks6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task 1: Write C# program to print all unique element in an array
+
+            //int[] array = { 1, 2, 3, 3, 2, 3, 7, 7 };
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    bool isUnique = true;
+
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        if (array[i] == array[j])
+            //        {
+            //            isUnique = false;
+            //            break;
+            //        }
+            //    }
+
+            //    if (isUnique)
+            //    {
+            //        Console.WriteLine(array[i]);
+            //    }
+            //}
+
+
+
+            //Task 2: Write C# program to copy all elements of one array to another
+
+            //int[] firstArr = { 1, 2, 3, 4, 5, 6, 7 };
+            //int[] secondArr = new int[firstArr.Length];
+
+            //for (int i = 0; i < firstArr.Length; i++)
+            //{
+            //    secondArr[i] = firstArr[i];
+            //}
+            //foreach (int i in secondArr)
+            //    Console.WriteLine(i);
+
+
+
+            //Task 3: Write C# program to count number of each element in an array
+
+            //int[] arr = {1, 2, 3, 3, 4, 5, 2, 3};
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    int count = 0;
+            //    for (int j = 0; j < arr.Length; j++) 
+            //    {
+            //        if (arr[i] == arr[j])
+            //            count++; 
+            //    }
+            //    Console.WriteLine($"Count of {arr[i]} = {count}");
+            //}
+
+        }
+    }
+}
